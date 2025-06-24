44f60eca3f1db73b84698e91ff11498dfcea3cb1
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-06-24 13:04:03 -0500

    Add files via upload

--- foobar.js
+++ foobar.js
@@ -1,14 +1,34 @@
 let storage = require("storage");
+let math = require("math");
 
-function tiles(my_rom, ranges, tile_size, tile_bytes) {
+function sum(data)
+{
+    let hits = 0;
+    for (let i = 0; i < data.length; i++)
+    {
+        if (typeof data[i] === "number")
+        {
+            hits += data[i];
+        }
+    }
+    return hits;
+}
+
+function mean(data)
+{
+    return sum(data) / data.length;
+}
+
+function extract_tiles(my_rom, ranges, tile_size, tile_bytes)
+{
     tile_size = tile_size || 8;
     tile_bytes = tile_bytes || 16;
     let palette = [[255,255,255], [192,192,192], [96,96,96], [0,0,0]];
-    //let tiles = [];
     let count = 0;
 
+    storage.makeDirectory("/ext/temp_rom_data")
+    
     let file = storage.openFile(my_rom, "r", "open_existing");
-    storage.makeDirectory("/ext/rom_data")
     
     for (let i = 0; i < ranges.length; i++)
     {
@@ -21,9 +41,8 @@ function tiles(my_rom, ranges, tile_size, tile_bytes) {
             count += 1;
             file.seekAbsolute(pos);
             let raw = file.read("ascii", tile_bytes);
-
             let tile = [];
-            let new_file = storage.openFile("/ext/rom_data/tile_" + count.toString(), "w", "create_always");
+            let new_file = storage.openFile("/ext/temp_rom_data/tile_" + count.toString() + ".txt", "w", "create_always");
 
 
             for (let y = 0; y < tile_size; y++)
@@ -45,24 +64,78 @@ function tiles(my_rom, ranges, tile_size, tile_bytes) {
             }
           
             new_file.close();
-
             print("Extracted tile: " + count.toString());
-
-            //tiles.push(tile);
             pos += tile_bytes;
         }
     }
 
     file.close();
-    return tiles;
+}
+
+function convert_tiles(my_rom)
+{
+    storage.makeDirectory("/ext/rom_data")
+    
+    for (let i = 1; i <= 384; i++)
+    {
+        let new_tile = [];
+        let temp = [];
+        let file = storage.openFile("/ext/temp_rom_data/tile_" + i.toString() + ".txt", "r", "open_existing");
+        for (let j = 0; j < 64; j++)
+        {
+            let data = file.read("ascii", 1);
+            if (j % 8 === 0)
+            {
+                if (temp.length > 0)
+                {
+                    new_tile.push(temp);
+                    temp = [];
+                }
+            }
+            else
+            {
+                if (data === "2" || data === "3")
+                {
+                    temp.push(1);
+                }
+                if (data === "0" || data === "1")
+                {
+                    temp.push(0);
+                }
+            }
+         }
+
+        if (temp.length > 0)
+        {
+            new_tile.push(temp);
+            temp = [];
+        }
+
+        let new_file = storage.openFile("/ext/rom_data/tile_" + i.toString() + ".txt", "w", "create_always");
+        for (let row = 0; row < 8; row += 2)
+        {
+            for (let col = 0; col < 8; col += 2)
+            {
+                let tile = [new_tile[row][col], new_tile[row][col + 1], new_tile[row + 1][col], new_tile[row + 1][col + 1]];
+                new_file.write(math.trunc(mean(tile)).toString());
+            }
+            new_file.write("\n");
+        }
+
+        print("Converted tile: " + i.toString());
+    }
+
+    storage.rmrf("/ext/temp_rom_data")
 }
 
 let tile_size = 8;
 let tile_bytes = 16;
-let ranges = [[0x8000, 0x87FF], [0x8800, 0x8FFF]];
+let ranges = [[0x8000, 0x87FF], [0x8800, 0x8FFF], [0x9000, 0x97FF]];
 let my_rom = "/ext/kirby.gb";
 print("Extracting tiles.");
-tiles(my_rom, ranges, tile_size, tile_bytes);
+extract_tiles(my_rom, ranges, tile_size, tile_bytes);
+print("Converting tiles.");
+convert_tiles(my_rom, ranges);
 
 //let gui = furi_record_open("gui");
 //let canvas = gui_direct_draw_acquire(gui);
