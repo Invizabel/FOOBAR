0b1ac3a24bcc0a277c14200b581c22e510c89601
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-07-07 07:21:14 -0500

    Add files via upload

--- foobar.js
+++ foobar.js
@@ -1,14 +1,8 @@
 let storage = require("storage");
 
-function opcode()
+function op_00()
 {
-    let hits = {};
-    hits[0x00] = function nop(){ PC++; return 4 };
-    hits[0x01] = ld16(B,C,Immediate);
-    hits[0x02] = ld_to_mem(B,C,A);
-    hits[0x03] = inc16(B,C);
-    hits[ 0x04 ] = inc(B);
-    return hits;
+    return function nop(){ PC++; return 4 };
 }
 
 opcode();
