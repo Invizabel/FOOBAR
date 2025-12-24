1b9e2fe5b528c6c8f40196e4050270f12618d51f
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-12-23 19:40:13 -0600

    Add files via upload

--- src/jelly.cpp
+++ src/jelly.cpp
@@ -1,5 +1,8 @@
+#include "dolphin/dolphin.h"
 #include <furi.h>
 #include <furi_hal.h>
+#include <gui/gui.h>
+#include <stdbool.h>
 #include <storage/storage.h>
 
 extern "C"
@@ -7,27 +10,96 @@ extern "C"
     #include "jelly.h"
 }
 
-int main()
+bool is_title = false;
+char title_buffer[16];
+
+title_array fetch_title()
 {
-    Storage * storage = (Storage*)furi_record_open("storage");    
+    Storage * storage = (Storage*)furi_record_open("storage");
     File* rom_file = storage_file_alloc(storage);
     uint8_t ROM[512];
     title_array t = {0};
-    if(storage_file_open(rom_file, "/ext/kirby.gb", FSAM_READ, FSOM_OPEN_EXISTING))
+    if(storage_file_open(rom_file, "/ext/roms/Faceball 2000.gb", FSAM_READ, FSOM_OPEN_EXISTING))
     {
         storage_file_read(rom_file, ROM, 512);
-        t = read_title(ROM);
-    }
-    
-    
-    if(storage_file_open(rom_file, "/ext/kirby.txt", FSAM_WRITE, FSOM_OPEN_ALWAYS))
-    {
-        storage_file_write(rom_file, &t, sizeof(t));
+        bool is_valid = verify_checksum(ROM);
+        if (is_valid)
+        {
+            t = read_title(ROM);
+        }
     }
 
     storage_file_close(rom_file);
     storage_file_free(rom_file);
     furi_record_close("storage");
+
+    return t;
+}
+
+static void input_callback(InputEvent* event, void* context)
+{
+    FuriMessageQueue* queue = (FuriMessageQueue*)context;
+    if(event->type == InputTypeShort || event->type == InputTypeRepeat || event->type == InputTypePress)
+    {
+    }
+
+    if(event->type == InputTypeRelease)
+    {
+    }
+    
+    
+    furi_message_queue_put(queue, event, FuriWaitForever);
+}
+
+void draw_callback(Canvas* canvas, void* context)
+{
+    UNUSED(context);
+
+    if (!is_title)
+    {
+        title_array t = fetch_title();
+        for (int i = 0; i < 16; i++)
+        {
+            title_buffer[i] = t.title[i];
+        }
+        is_title = true;
+    }
+    
+    canvas_clear(canvas);
+    canvas_draw_str(canvas,2,8,title_buffer);
+
+    canvas_commit(canvas);
+}
+
+int main()
+{
+    FuriMessageQueue* queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+    ViewPort* view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, draw_callback, NULL);
+    view_port_input_callback_set(view_port, input_callback, queue);
+    Gui* gui = (Gui*)furi_record_open("gui");
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+    dolphin_deed(DolphinDeedPluginGameStart);
+    InputEvent event;
+    bool running = true;
+    while(running)
+    {
+        if(furi_message_queue_get(queue, &event, FuriWaitForever) == FuriStatusOk)
+        {
+            if(event.type == InputTypeShort && event.key == InputKeyBack)
+            {
+                running = false;
+            }
+        }
+        view_port_update(view_port);
+    }
+
+    view_port_enabled_set(view_port, false);
+    furi_message_queue_free(queue);
+    gui_remove_view_port(gui, view_port);
+    view_port_free(view_port);
+    furi_record_close(RECORD_GUI);
+    return 0;
     
     return 0;
 }
--- src/jelly.h
+++ src/jelly.h
@@ -5,10 +5,17 @@
 extern "C" {
 #endif
 
+#include <stdbool.h>
+
 typedef struct
 {
     char title[16];
 }   title_array;
+
+typedef struct
+{
+    char array[256];
+}   BasicArray;
 
 title_array read_title(uint8_t ROM[512])
 {
@@ -19,12 +26,28 @@ title_array read_title(uint8_t ROM[512])
         uint8_t c = ROM[0x0134 + i];
         if (c == 0x00)
         {
+            t.title[count++] = '\n';
             break;
         }
         
         t.title[count++] = (char)c;
     }
     return t;
+}
+
+bool verify_checksum(uint8_t ROM[512])
+{
+    uint8_t checksum = 0;
+    for (uint16_t address = 0x0134; address <= 0x014C; address++)
+    {
+        checksum = checksum - ROM[address] - 1;
+    }
+    if (checksum == ROM[0x014D])
+    {
+        return true;
+    }
+
+    return false;
 }
 
 typedef struct
