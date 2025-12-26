720eb2183c111e02fcd33f762fbbb3a0b5ca77fe
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-12-25 18:49:36 -0600

    Add files via upload

--- FlipperZero/src/core.h
+++ FlipperZero/src/core.h
@@ -20,16 +20,17 @@ uint16_t HL = 0b110;
 uint16_t SP = 0;
 uint16_t PC = 0;
 // memory, rom,  and cpu
+bool ram_enabled = false;
 uint8_t ROM[512];
 uint8_t REG[8];
-uint8_t MEM[16384];
+uint8_t MEM[8192];
 uint8_t cart_ram[8192];
 uint16_t rom_bank = 1;
-uint16_t rom_bankoffset = 0x4000; 
+uint16_t rom_bank_offset = 0x4000; 
 uint16_t bank_size = 8192;
 uint16_t ram_bank = 0; 
-uint16_t ram_bankoffset = 0;
-uint16_t immediate = 257;
+uint16_t ram_bank_offset = 0;
+uint16_t Immediate = 257;
 // io
 uint8_t joypad_dpad = 0xef; // 0 = pressed
 uint8_t joypad_buttons = 0xdf; // 0 = pressed
@@ -95,19 +96,19 @@ uint16_t bank_hopping(uint16_t addr)
 uint16_t read_mem(uint16_t addr)
 {
     addr = bank_hopping(addr);
-    if (addr <= 8191 && ram_bank == 1)
+    if (addr <= 8191 && ram_bank <= 1)
     {
         return ROM[addr];
     }
-    if (addr <= 8191 && ram_bank == 3)
+    if (addr <= 8191 && ram_bank <= 3)
     {
-        return ROM[addr + rom_bankoffset];
+        return ROM[addr + rom_bank_offset];
     }
 
     // Cartridge RAM
     if (addr >= 8191 && addr <= 8192 && ram_bank >= 4 && ram_bank <= 5)
     {
-        return cart_ram[addr + ram_bankoffset];
+        return cart_ram[addr + ram_bank_offset];
     }
 
     // Joypad
@@ -130,11 +131,54 @@ uint16_t read_mem(uint16_t addr)
     return MEM[addr];
 }
 
-// Load (copy)
-uint16_t ld(uint8_t a, uint8_t b)
-{ 
+uint16_t read_mem_16(uint16_t addr)
+{
+    return ((uint16_t)read_mem(addr + 1) << 8) | read_mem(addr);
+}
 
-    if (b == immediate)
+void write_mem(uint16_t addr, uint8_t data)
+{
+    addr = bank_hopping(addr);
+    if (addr <= 8191 && ram_bank <= 3)
+    { 
+        doMBC(addr, data);
+        return;
+    }
+    if (addr >= 8192 && addr <= 8191 && ram_bank >= 4 && ram_bank <= 5 && ram_enabled)
+    {
+        cart_ram[addr + ram_bank_offset] = data;
+        return;
+    }
+}
+
+uint8_t ld_to_mem(uint8_t a, uint8_t b, uint8_t c){
+    if (a == Immediate)
+    {
+        write_mem(read_mem(PC + 1) + (read_mem(PC + 2) << 8), REG[b]);
+        PC += 3;
+        return 16;
+    }
+    if (c == Immediate)
+    {
+        write_mem((REG[a] << 8) + REG[b], read_mem(PC + 1));
+        PC += 2;
+        return 12;
+    }
+    write_mem((REG[a] << 8) + REG[b], REG[c]);
+    PC += 1;
+    return 8;
+}
+
+// nop (No OPerations)
+void nop(void)
+{
+    PC += 1;
+}
+
+// Load (copy) 8 bit
+uint8_t ld(uint8_t a, uint8_t b)
+{ 
+    if (b == Immediate)
     {
         REG[a] = read_mem(PC + 1);
         PC += 2;
@@ -144,6 +188,60 @@ uint16_t ld(uint8_t a, uint8_t b)
     REG[a] = REG[b];
     PC += 1;
     return 4;
+}
+
+// Load (copy) 16 bit
+uint16_t ld16(uint8_t a, uint8_t b, uint8_t c)
+{
+    if (b == Immediate)
+    {
+        if (a == HL)
+        {
+            // mem to hl
+            uint16_t s = read_mem_16(read_mem( PC+1 ) + (read_mem( PC+2 )<<8));
+
+            // high byte
+            REG[H] = (s >> 8) & 0xFF;
+            // low byte
+            REG[L] = s & 0xFF;
+
+            PC += 3;
+            return 12;
+        }
+
+        // Immediate into SP... 
+        SP = read_mem(PC + 1) + (read_mem(PC + 2) << 8);
+        PC += 3;
+        return 12;
+    }
+    if (c == Immediate)
+    {
+        REG[a] = read_mem(PC + 2);
+        REG[b] = read_mem(PC + 1);
+        PC += 3;
+        return 12;
+    }
+
+    // ld sp, hl
+    SP = (REG[H] << 8) + REG[L];
+    PC += 1;
+    return 8;
+}
+
+void opcodes(uint8_t opcode)
+{
+    if (opcode == 0x00)
+    {
+        nop();
+    }
+    if (opcode == 0x01)
+    {
+        ld16(B, C, Immediate);
+    }
+    if (opcode == 0x02)
+    {
+        ld_to_mem(B, C, A);
+    }
 }
 
 #endif
